d223f154b2f71a9a3d33e61e054b150ee8d1b827
Roleen Nunes <roleen.nunes@mail.utoronto.ca>
2017-10-16 17:33:17 -0400

    Partially completed Lab5 prelab question 3

--- Labs/5/prelab_q2.s
+++ Labs/5/prelab_q2.s
@@ -1,8 +1,8 @@
 .equ ADDR_JP2, 0xFF200070     # address GPIO JP2
-.equ ADDR_JP2_IRQ, 0x1000      # IRQ line for GPIO JP2 (IRQ12) 
+.equ ADDR_JP2_IRQ, 0x1000      # IRQ line for GPIO JP2 (IRQ12)
 
 movia  r8, ADDR_JP2         # load address GPIO JP2 into r8
-movia  r9, 0x07f557ff       # set motor,threshold and sensors bits to output, set state and sensor valid bits to inputs 
+movia  r9, 0x07f557ff       # set motor,threshold and sensors bits to output, set state and sensor valid bits to inputs
 stwio  r9, 4(r8)
 
 # load sensor0 threshold value 5 and enable sensor0
@@ -20,11 +20,11 @@ stwio  r9,  0(r8)
 movia  r12, 0x8000000       # enable interrupts on sensor 0
 stwio  r12, 8(r8)
 
-movia  r8, ADDR_JP2_IRQ    # enable interrupt for GPIO JP2 (IRQ12) 
+movia  r8, ADDR_JP2_IRQ    # enable interrupt for GPIO JP2 (IRQ12)
 wrctl  ctl3, r8
 
 movia  r8, 1
-wrctl  ctl0, r8            # enable global interrupts 
+wrctl  ctl0, r8            # enable global interrupts
 
 LOOP:
 br LOOP
---
+++ Labs/5/prelab_q3.s
@@ -0,0 +1,33 @@
+.equ TIMER, 0xFF202000
+.equ TIMER_IRQ, 0x1
+.equ TIME, 100000
+
+.global _start
+_start:
+    # enable timer interrupt
+    movia  r8, TIMER_IRQ    # enable interrupt for GPIO JP2 (IRQ12)
+    wrctl  ctl3, r8
+    movia  r8, 1
+    wrctl  ctl0, r8             # enable global interrupts
+
+    # Set timer
+    movia r7, TIMER        # r7 contains the base address for the timer 
+    movui r2, TIME
+    stwio r2, 8(r7)             # Set the period to be TIME clock cycles 
+    stwio r0, 12(r7)
+    movui r2, 5
+    stwio r2, 4(r7)             # Start the timer without continuing with interrupt enabled
+
+.section .exceptions, "ax"
+
+IHANDLER:
+    rdctl et, ctl4          # check for hardware interrupt
+    andi et, et, TIMER_IRQ        # check if interrupt pending from IRQ0
+    beq et, r0, EXIT_IHANDLER   # if not, exit handler
+
+    # code to handle interrupt from IRQ0
+    # code to acknowledge interrupt from IRQ0
+
+EXIT_IHANDLER:
+    subi ea, ea, 4
+    eret
